addb35546b5f9909b2c4f7db56ece57703814495
Busya <92587046+Krylovich@users.noreply.github.com>
2022-05-02 23:09:40 +0300

    feat: add nice try

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(backyard) {
+    backyard = [];
+   var number = {};
+   backyard.forEach(function(catEars) {
+     if(number[catEars] == "^^") {
+       ++number[catEars]
+     } else {
+       number[catEars] = 0;
+     }
+   });
+   return number
 }
 
 module.exports = {
